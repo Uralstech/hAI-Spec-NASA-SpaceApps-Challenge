57aca5dd8196b357ed8ec82dfffb2c7de8369c08
Udayshankar Ravikumar <uralstech@gmail.com>
2023-10-07 16:01:07 +0530

    Updated documentation for scripts.

--- src/main.py
+++ src/main.py
@@ -13,22 +13,49 @@ from shutil import rmtree
 HERE: str = dirname(abspath(__file__))
 CACHE: str = join(HERE, ".DocsCache/")
 
+# Older version of a standard
 PDF_1: str = "https://standards.nasa.gov/sites/default/files/standards/NASA/B/0/Historical/nasa_std_5009.pdf"
+
+# Next version of the standard
 PDF_2: str = "https://standards.nasa.gov/sites/default/files/standards/NASA/B/0/Historical/nasa-std-5009a.pdf"
+
+# Get name, filepath of PDF 1 and 2
 PDF_1_NAME: str = PDF_1.split("/")[-1]
 PDF_2_NAME: str = PDF_2.split("/")[-1]
 PDF_1_PATH: str = join(CACHE, PDF_1_NAME)
 PDF_2_PATH: str = join(CACHE, PDF_2_NAME)
+
+# Get path for output dir and names for the prompt files.
 OUTPUT_DIR = join(HERE, "Outputs/")
 OUTPUT: str = join(OUTPUT_DIR, "Prompt_{0}.txt")
 
-def cache_pdf(pdf: str, path: str):
+# Downloads the pdf.
+def cache_pdf(pdf: str, path: str) -> None:
+    """
+    Downloads the standard's PDF from the NASA website.
+
+    Args:
+        pdf (str): The link to the PDF.
+        path (str): The path to store the PDF.
+    """
     request: Response = get(pdf, stream=True)
     with open(path, "wb") as file:
         for chunk in request.iter_content(None, True):
             file.write(chunk)
 
 def format_pdf(pdf: str) -> dict[str, str]:
+    """
+    Formats the standard's PDF into a dictionary of sections.
+
+    Args:
+        pdf (str): The path to the PDF.
+    
+    Returns:
+        dict[str, str] where:
+            Key -> Section name, e.g. "SCOPE", "APPLICABLE DOCUMENTS".
+            Value -> Content of the section.
+    """
+
     with open(pdf, 'rb') as document:
         # Create resource manager
         resource_manager: PDFResourceManager = PDFResourceManager()
@@ -84,6 +111,7 @@ def format_pdf(pdf: str) -> dict[str, str]:
 
     return sections
 
+# Check if the folders and files we need exist
 if not isdir(CACHE):
     mkdir(CACHE)
 
@@ -99,6 +127,7 @@ if not isfile(PDF_1_PATH):
 if not isfile(PDF_2_PATH):
     cache_pdf(PDF_2, PDF_2_PATH)
 
+# Format the standards
 pdf_1: dict[str, str] = format_pdf(PDF_1_PATH)
 pdf_2: dict[str, str] = format_pdf(PDF_2_PATH)
 
@@ -106,7 +135,9 @@ index: int = 0
 pdf_2_sections = pdf_2.keys()
 for key, value in pdf_1.items():
     for section in pdf_2_sections:
+        # Check if the section from v-A is there in v-B
         if key.split(",")[0].split()[0].strip() in section:
+            # If so, write a new prompt file.
             with open(str.format(OUTPUT, index), "w", encoding="utf-8") as file:
                 file.write(f"I am making an AI and need a dataset. I have these two pages of different versions of a document.\n\nVERSION A:\n{pdf_1[key]}\n\nVERSION B:\n{pdf_2[section]}\n\nI want you to compare the two versions, then create a dataset entry in the format described below:\n\n\\### Instruction:\n[Please put the page of version A here]\n\n\\### Output:\nSuggesting Changes: [Yes / No, depending on if there are changes between the pages]\nCurrent Language: [The language in version A that has been changed in version B]\nSuggested Language: [The language in version B that replaced the language in version A]\nReason For Change: [The possible reason for the change between version A and B]\n\nPlease set \"Suggested Changes\" as \"No\" if the two pages are completely different and not related in any way.")
             index += 1
